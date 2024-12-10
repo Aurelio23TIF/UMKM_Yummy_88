35be79ece8e6fb117ca860e6ba421e679a8bcb2c
muthiaadeis <muthia23ti@mahasiswa.pcr.ac.id>
2024-12-10 21:39:27 +0700

    Edit form login

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-{{-- <img src="{{ asset('build/assets/logo.png') }}" alt="New Logo" class="h-12 w-auto"> --}}
+<img src="{{ asset('build/assets/logo.png') }}" alt="New Logo" class="h-12 w-auto">
 
 {{-- <div class="text-center mb-4">
     <img src="{{ asset('build/assets/logo.png') }}" alt="Logo" style="width: 80px;">
